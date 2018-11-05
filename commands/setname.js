75b569d194b0dc62e14e06bc0704d825ff8ab345
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-05 18:45:42 -0500

    Update setname.js

--- commands/setname.js
+++ commands/setname.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.roles.get("509142932968701952")) return message.reply("Invalid permissons! You must have the `Bot Permission` role."){
+   let allowedid = message.guild.roles.get("509142932968701952");
+	if(message.member.roles.has(role.id)) {
 	    let olduser = bot.user.username;
 	    let newuser = args.join(" ");
 	    bot.user.setUsername(args.join(" ")).then(() => {message.reply(`New username set to **${args.join(" ")}**!`); bot.guilds.get('503074702902689803').channels.get('506303108998234133').send(`My username has been changed from **${olduser}** to **${newuser}** by **${message.author.tag}**.`)})
