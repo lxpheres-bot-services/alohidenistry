dd89e696bd1fb6a5136212957c0bb18d34ea2c8c
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-05 18:58:11 -0500

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const fs = require("fs"); 
+
+module.exports.run = async (bot, message, args) => {
+	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("it doesn't look like you can use that!");
+	let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	if (!wUser) return message.reply("That member can't be found!");
+	if (wUser.hasPermission("MANAGE_MESSAGES") || wUser.hasPermission("ADMINISTRATOR")) return message.reply("Cannot warn that user! Check that I have sufficent permissions, or you have permission to warn that user!");
+	let reaso = args.shift();
+	let reason = args.join(" ");
+
+	if (!reason) {reason = "*No reason specified.*"}
+  message.channel.send(`**${message.author.username}** warned <@${wUser.id}> because of **${reason}**.`);
+	wUser.send(`You have been warned in **${message.guild.name}** for **${reason}**.`);
+	
+}
+
+module.exports.help = {
+	name: "warn",
+	usage: "warn <user> [reason]",
+	description: "Warns a user",
+	longdes: "Warns the user given along with a reason.",
+	mentionedperm: "MANAGE_MESSAGES",
+  category: "Moderation"
+}
