08c5a226fa468a16d943a2c76eef75aed7e751de
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-05 18:50:27 -0500

    Update setname.js

--- commands/setname.js
+++ commands/setname.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-   let allowedid = message.guild.roles.get("509142932968701952");
-	if(message.member.roles.has(role.id)) {
-	    let olduser = bot.user.username;
+ let allowedRole = message.guild.roles.find("name", "Bot Permissions");
+    if (message.member.roles.has(allowedRole.id) {
+            let olduser = bot.user.username;
 	    let newuser = args.join(" ");
 	    bot.user.setUsername(args.join(" ")).then(() => {message.reply(`New username set to **${args.join(" ")}**!`); bot.guilds.get('503074702902689803').channels.get('506303108998234133').send(`My username has been changed from **${olduser}** to **${newuser}** by **${message.author.tag}**.`)})
 	    	.catch((error) => {message.reply(`Can not set username to **${newuser}** because of \`${error}\``); bot.guilds.get('503074702902689803').channels.get('506303108998234133').send(`Set username command ran by **${message.author.tag}** failed with error: \`${error}\`.`)})
