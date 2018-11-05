aa6f0278df30607f743570da722367c8ccc14453
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-05 18:54:16 -0500

    Update setname.js

--- commands/setname.js
+++ commands/setname.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
- let allowedRole = message.guild.roles.find("name", "Bot Permissions");
-    if (message.member.roles.has(allowedRole.id) 
+    const allowedid = ['293060399106883584','290111320857116672'];
+    if (allowedid.includes(message.author.id)) {
+
             let olduser = bot.user.username;
 	    let newuser = args.join(" ");
 	    bot.user.setUsername(args.join(" ")).then(() => {message.reply(`New username set to **${args.join(" ")}**!`); bot.guilds.get('503074702902689803').channels.get('506303108998234133').send(`My username has been changed from **${olduser}** to **${newuser}** by **${message.author.tag}**.`)})
