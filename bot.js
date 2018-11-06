f6ee4bf98c8e76663e0477d60c74ab585be48eae
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-06 18:16:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs")
+const fs = require("fs");
 const rbx = require("noblox.js");
 
 client.commands = new Discord.Collection();
