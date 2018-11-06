a09216671fa91592c1c28d359b0a234b346d001d
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-06 10:36:02 -0500

    Create up.js

---
+++ commands/up.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js");
+const fs = require("fs"); 
+
+module.exports.run = async (bot, message, args) => {
+    const client = bot;
+    
+    let totalSeconds = (client.uptime / 1000);
+    let weeks = Math.floor(totalSeconds / 604800);
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.round(totalSeconds % 60);
+    
+    let up = `${weeks}w ${days}d ${hours}h ${minutes}m ${seconds}s`;
+    message.channel.send({embed: {
+    color: 15158332,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "\n",
+    description: "\n",
+    fields: [{
+        name: "Dart Bot's Uptime",
+        value: up,
+        inline: true
+      },
+  ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: `${message.author.avatarURL}`,
+      text: `Command ran by ${message.author.tag}`
+    }
+  }
+});
+}
+
+module.exports.help = {
+	name: "up",
+	usage: "up",
+	description: "nil",
+	longdes: "Uptime of bot.",
+	mentionedperm: "None",
+  category: "Utility"
+}
