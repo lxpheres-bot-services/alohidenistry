13d91f9728edc9fa918c0cdb8bc437e2ef437439
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-06 17:52:54 -0500

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -3,30 +3,33 @@ module.exports = {
 	run: async (bot, message, args) => {
 		if (!message.member.roles.get("509497944043552768")) return message.reply("Invalid permissons! You must have the `Bot Admin` role.").;
 		if (!args[1]) return message.reply("Usage: `;rank (username) (rank)`");
-		rbx.getRoles(4034813).then((roles) => {
+		rbx.getRoles(3008227).then((roles) => {
 			rbx.getIdFromUsername(args[0]).then((result) => {
-				rbx.getRankInGroup(4034813, result).then((currentRank) => {
-					if (currentRank === 0) return message.reply("This person isn't in the group!").catch(() => bot.safeSend(message, module.exports.help.name));
+				rbx.getRankInGroup(3008227, result).then((currentRank) => {
+					if (currentRank === 0) return message.reply("This person isn't in the group!");
 					var rank = args.splice(1, args.length).join(" ");
 					if (!roles.find((role) => role.Name === rank)) return message.reply("Not a valid rank name!").catch(() => bot.safeSend(message, module.exports.help.name));
-					if (roles.find((role) => role.Name === rank).Rank > roles.find((role) => role.Name === "President").Rank || currentRank > roles.find((role) => role.Name === "President").Rank) return message.reply("You cannot change the rank of a President+, or rank anyone above President.").catch(() => bot.safeSend(message, module.exports.help.name));
-					rbx.setRank(4034813, result, roles.find((role) => role.Name === rank).Rank).then(() => {
-						message.reply(`Ranked \`${args[0]}\` to \`${rank}\``).catch(() => bot.safeSend(message, module.exports.help.name));
+					if (roles.find((role) => role.Name === rank).Rank > roles.find((role) => role.Name === "Vice President").Rank || currentRank > roles.find((role) => role.Name === "Vice President").Rank) return message.reply("You cannot change the rank of a Vice President+ or rank anyone above Vice President.");
+					rbx.setRank(3008227, result, roles.find((role) => role.Name === rank).Rank).then(() => {
+						message.reply(`Ranked \`${args[0]}\` to \`${rank}\``);
 					}).catch(() => {
-						message.reply("Couldn't rank this user in the group.").catch(() => bot.safeSend(message, module.exports.help.name));
+						message.reply("Couldn't rank this user in the group.");
 					});
 				}).catch(() => {
-					message.reply("Couldn't fetch this user's rank.").catch(() => bot.safeSend(message, module.exports.help.name));
+					message.reply("Couldn't fetch this user's rank.");
 				});
 			}).catch(() => {
-				message.reply("Couldn't find this user.").catch(() => bot.safeSend(message, module.exports.help.name));
+				message.reply("Couldn't find this user.");
 			});
 		}).catch(() => {
-			message.reply("Couldn't fetch group ranks.").catch(() => bot.safeSend(message, module.exports.help.name));
+			message.reply("Couldn't fetch group ranks.");
 		});
 	},
-	help: {
-		name: "rank",
-		category: "Group Administration"
-	}
-};
+module.exports.help = {
+name: "rank",
+usage: ";rank plr rank",
+description: "Ranks someone in the roblox group.",
+longdes: "Ranks someone in the roblox group.",
+mentionedperm: "Bot Admin Role",
+category: "Utility"
+}
