19288c026a9e1a8b4751c2087e393336f83fdc6f
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-06 17:54:43 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs")
+const rbx = require("noblox.js");
+
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands", (err, files) => {
+	if (err) console.log(err);
+	let jsfile = files.filter(f => f.split(".").pop() === "js")
+	if (jsfile.length <= 0) {
+		console.log("Couldn't find any commands!")
+		return
+	}
+
+	jsfile.forEach((f, i) => {
+		let props = require(`./commands/${f}`);
+		console.log(`${f} has been loaded!`);
+		client.commands.set(props.help.name, props);
+	});
+});
+
+client.on("ready", async () => {
+	console.log(`${client.user.tag} has started!`);
+	 rbx.login("CreamyzRanking", process.env.rblx);
+});
+
+client.on("message", async message => {
+
+	if (message.author.bot) return;
+
+	let prefix = ";";
+	
+	if(message.content.indexOf(prefix) !== 0) return;
+	
+	
+	let messageArray = message.content.split(" ");
+	let cmd = messageArray.shift();
+	let args = messageArray
+
+	let commandfile = client.commands.get(cmd.slice(prefix.length));
+	if (commandfile) commandfile.run(client,message,args);
+});
+
+client.login(process.env.BOT_T0KEN);
