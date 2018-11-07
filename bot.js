b0987892ada1f603fd5ef040c1e32efbba9057b1
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-06 19:05:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
-const rbx = require("./noblox.js");
+const rbx = require("noblox.js");
 module.exports.run = async () => {
 	const bot = new Discord.Client({ disableEveryone: true, fetchAllMembers: true });
 
