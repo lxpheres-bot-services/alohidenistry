11b6733d51ada0d4ad3605075f94dd796b7d2fd0
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-07 15:37:43 -0500

    Update shout.js

--- commands/shout.js
+++ commands/shout.js
@@ -1,7 +1,7 @@
 var rbx = require("noblox.js");
 module.exports = {
 	run: async (bot, message, args) => {
-		if (!message.member.hasPermission("MANAGE_ROLES") || message.guild.id !== "503074702902689803" return message.reply("Invalid permissons! You must have the `MANAGE_ROLES` permission.").catch(() => bot.safeSend(message, module.exports.help.name));
+		if (!message.member.hasPermission("MANAGE_ROLES") || message.guild.id !== "503074702902689803" message.reply("Invalid permissons! You must have the `MANAGE_ROLES` permission.").catch(() => bot.safeSend(message, module.exports.help.name));
 		if (!args[0]) return message.reply("Usage: `;shout (message)`").catch(() => bot.safeSend(message, module.exports.help.name));
 		rbx.shout(3008227, args.join(" ")).then(() => {
 			message.reply("Successfully shouted to the group.").catch(() => bot.safeSend(message, module.exports.help.name));
