60ffc8ef19aa56ebce24e87a60f5196e3fc11076
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-19 17:23:23 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -5,12 +5,12 @@ module.exports.run = async (bot, client, response, args) => {
   message = response;
   const allowedid = ['293060399106883584'];
    if (allowedid.includes(message.author.id)) {
-	    return response.reply("Usage: `+setstatus (status: online, idle, invisible, dnd) (type: playing, listening, watching) (description)`");
+	    return response.reply("Usage: `;setstatus (status: online, idle, invisible, dnd) (type: playing, listening, watching) (description)`");
 		bot.user.client.user.setPresence({ game: { type: args[1].toUpperCase(), name: args.splice(2, args.length).join(" ") }, status: args[0].toLowerCase() }).then(() => {
 			message.reply(":white_check_mark: All set!").catch(() => bot.safeSend(message, module.exports.help.name));
 		}).catch(() => {
 			message.reply(":x: Couldn't change the status!").catch(() => bot.safeSend(message, module.exports.help.name));
-		});
+		};
 }
 
 module.exports.help = {
