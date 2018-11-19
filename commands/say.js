b39cc2d8c79e1a2ff540897631ffe356fedc1d91
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-19 15:17:35 -0500

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+    if (message.member.hasPermission("MANAGE_SERVER")) {
+      const sayMessage = args.join(" ");
+      message.channel.send(sayMessage);
+      message.delete();
+    } else {message.reply("Error! You do not have permission to use this command! You need the `MANAGE_SERVER` permission!")}
+}
+
+module.exports.help = {
+	name: "say",
+	usage: "say <message>",
+	description: "Sends the message as a bot.",
+	longdes: "Sends the message given in the command as the bot.",
+	mentionedperm: "MANAGE_SERVER",
+  category: "Utility"
+}
