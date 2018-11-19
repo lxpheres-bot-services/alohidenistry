c14ce356f43deb7bef1f0b66d2057c13a9e16bc9
ethanlaj <ethanlaj@ericlaj.com>
2018-11-19 18:02:14 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -4,10 +4,13 @@ const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
   const allowedid = ['293060399106883584'];
    if (allowedid.includes(message.author.id)) {
+	let type = args[1].toUpperCase();
+	if (type === "PLAYING") type = 0;
+	else if (type === "LISTENING") type = 2;
+	else if (type === "WATCHING") type = 3;
 	if (!args[1]) return message.reply("Usage: `;setstatus (status: online, idle, invisible, dnd) (type: playing, listening, watching) (description)`");
-	bot.user.setPresence({ game: { type: args[1].toUpperCase(), name: args.splice(2, args.length).join(" ") }, status: args[0].toLowerCase() }).then(() => {
+	bot.user.setPresence({ game: { type: type, name: args.splice(2, args.length).join(" ") }, status: args[0].toLowerCase() }).then(() => {
 		message.reply(":white_check_mark: All set!").catch(() => bot.safeSend(message, module.exports.help.name));
-		console.log(bot.user.presence);
 	}).catch(() => {
 		message.reply(":x: Couldn't change the status!").catch(() => bot.safeSend(message, module.exports.help.name));
 	});
