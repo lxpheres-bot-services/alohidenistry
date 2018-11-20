ad4a10c391ea0b971cff5b7d91f1b5fe7a4d1465
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-20 15:33:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,12 +29,9 @@ client.on("ready", async () => {
 });
 
 client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find('name', 'bot-logs');
-  // Do nothing if the channel wasn't found on this server
   if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
+  channel.send(`Welcome to **Creamy'z Cafe | Offical Server**, ${member.user.tag}!`);
 });
 
 client.on("message", async message => {
