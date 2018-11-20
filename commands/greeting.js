fb6bd50492d7a55d1a6ea1a6afb4cfee7915ca06
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-20 15:25:20 -0500

    Update greeting.js

--- commands/greeting.js
+++ commands/greeting.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
 // Crasdsada
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('name', 'member-log');
+  const channel = member.guild.channels.find('name', 'bot-logs');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
