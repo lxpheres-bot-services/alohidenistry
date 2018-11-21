b1e76694e7ddc3bf3fd518b2e9cf897aa8f3c3b6
ethanlaj <ethanlaj@ericlaj.com>
2018-11-21 18:28:58 -0500

    Create eannounce.js

---
+++ commands/eannounce.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
+	let embed = new Discord.RichEmbed()
+		.setTitle("Server Announcement")
+		.setColor("#ffeb5c")
+		.setDescription(args.join(" "))
+		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL);
+	bot.channels.get("503757930173300737").send("@everyone", {embed: embed}).then(() => {
+		message.reply("Message sent!");
+	}).catch(() => {
+		message.reply("Something went wrong when announcing, please check my permissions and try again.");
+	});
+}
+module.exports.help = {
+	name: "eannounce",
+	usage: "eannounce <txt>",
+	description: "Announcement command using everyone.",
+	longdes: "An announcement command using everyone.",
+	mentionedperm: "MANAGE_ROLES",
+        category: "Utility"
+}
