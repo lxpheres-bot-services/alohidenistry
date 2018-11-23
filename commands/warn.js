4ff74161cf7a4949104ab03cb5ccf78c36a63336
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-23 18:41:30 -0500

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
 const fs = require("fs"); 
 
-module.exports.run = async (bot, client, response, args) => {
-	if (!response.member.hasPermission("MANAGE_MESSAGES")) return response.reply("It doesn't look like you can use that!");
-	let wUser = response.guild.member(response.mentions.users.first()) || response.guild.members.get(args[0]);
-	if (!wUser) return response.reply("That member can't be found!");
-	if (wUser.hasPermission("MANAGE_MESSAGES") || wUser.hasPermission("ADMINISTRATOR")) return response.reply("cannot warn that user! Check that I have sufficent permissions, or you have permission to warn that user!");
+module.exports.run = async (bot, client, message, args) => {
+	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("It doesn't look like you can use that!");
+	let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	if (!wUser) return message.reply("That member can't be found!");
+	if (wUser.hasPermission("MANAGE_MESSAGES") || wUser.hasPermission("ADMINISTRATOR")) return message.reply("Cannot warn that user! Check that I have sufficent permissions, or you have permission to warn that user!");
 	let reaso = args.shift();
 	let reason = args.join(" ");
 	if (!reason) {reason = "*No reason specified.*"}
-  require('../resources/embed.js').log("Moderation Action - Warn", `**User:** ${wUser.user.tag} \n**Moderator:** ${response.author.tag} \n**Reason:** ${reason}`, response)
-	wUser.send(`Hey! You have been warned in **${response.guild.name}** because of **${reason}**.`);
-  response.channel.send(`Okay, **${wUser.user.tag}** was warned for '**${reason}**', ${response.author.username}.`);
+  require('../resources/embed.js').log("Moderation Action - Warn", `**User:** ${wUser.user.tag} \n**Moderator:** ${message.author.tag} \n**Reason:** ${reason}`, response)
+	wUser.send(`Hey! You have been warned in **${message.guild.name}** because of **${reason}**.`);
+  message.channel.send(`Okay, **${wUser.user.tag}** was warned for '**${reason}**', ${message.author.username}.`);
 }
 
 module.exports.help = {
