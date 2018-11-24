c48d153e5cedea2e1d77a3e232dcbbaf9df31a94
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-23 19:21:37 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -17,8 +17,8 @@ module.exports.run = async (bot, message, args) => {
    const wUser = member;
 	
     await member.kick(reason).then( a => 
-    require('../resources/embed.js').log("Moderation Action - Kick", `**User:** ${wUser.user.tag} \n**Moderator:** ${message.author.tag} \n**Reason:** ${reason}`, response);
-    wUser.send(`Hey! You have been kicked from **${response.guild.name}** because of **${reason}**.`);
+    require('../resources/embed.js').log("Moderation Action - Warn", `**User:** ${wUser.user.tag} \n**Moderator:** ${message.author.tag} \n**Reason:** ${reason}`, message)
+    wUser.send(`Hey! You have been kicked from **${message.guild.name}** because of **${reason}**.`);
     message.channel.send(`Okay, ${wUser.user.tag} was kicked for ${reason}, ${message.author.username}.`);)
      .catch(error => message.channel.send(`Sorry, ${message.author.username}, I couldn't kick ${wUser.user.tag}.`));
 }
