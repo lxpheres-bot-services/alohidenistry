41e49bbcdf4b6daa7689dd1aa10e951a7539ca2a
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-01-30 16:57:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ module.exports.run = async () => {
 	});
 
 	client.on("ready", async () => {
-		client.user.setPresence({ game: { type: 3, name: "Creamy'z Cafe! | ;help" }, status: "online" });
+		client.user.setPresence({ game: { type: 3, name: "Bervo Hotels! | ;help" }, status: "online" });
 		console.log(`${client.user.tag} has started!`);
 		rbx.login({ username: "CreamyzRankingBot", password: process.env.rblx});
 	});
