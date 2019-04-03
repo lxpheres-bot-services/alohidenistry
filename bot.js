6bcb1aec398d980c27b146211cbc2897b996c89a
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-04-03 17:14:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ module.exports.run = async () => {
 	client.on("ready", async () => {
 		client.user.setPresence({ game: { type: 3, name: "Bervo Hotels! | ;help" }, status: "online" });
 		console.log(`${client.user.tag} has started!`);
-		rbx.login({ username: "CreamyzRankingBot", password: process.env.rblx});
+		//rbx.login({ username: "CreamyzRankingBot", password: process.env.rblx});
 	});
 
 	client.on("guildMemberAdd", (member) => {
