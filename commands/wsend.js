caae41cf04b43c8234b6cbc7a8fd71db8bde7369
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-04-04 15:47:33 -0400

    Update wsend.js

--- commands/wsend.js
+++ commands/wsend.js
@@ -1,14 +1,13 @@
 module.exports.run = async (bot, message) => {
 	const allowedid = ["293060399106883584"];
 	if (allowedid.includes(message.author.id)) {
-		const sayMessage = args.join(" ");
-		bot.guilds.get("559491866207453184").channels.get("559491866207453186").send(sayMessage);
+		const eMessage = args.join(" ");
+		bot.guilds.get("559491866207453184").channels.get("559491866207453186").send(eMessage);
 		message.delete();
-	} else {message.reply("Error! You do not have permission to use this command! You need to be the developer!");}
 };
 
 module.exports.help = {
-	name: "esend",
+	name: "wsend",
 	usage: "esend",
 	description: "Sends the message as a bot.",
 	longdes: "Sends the message given in the command as the bot.",
