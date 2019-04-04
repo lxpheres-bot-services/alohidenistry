c2e8cb16242f5fe6555be6479379cdcfb3868e26
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-04-04 15:51:01 -0400

    Update wsend.js

--- commands/wsend.js
+++ commands/wsend.js
@@ -1,6 +1,6 @@
-module.exports.run = async (bot, message) => {
+module.exports.run = async (bot, message, args) => {
 	const allowedid = ["293060399106883584"];
-	if (allowedid.includes(message.author.id)) {
+	if (allowedid.includes(message.author.id))
 		const eMessage = args.join(" ");
 		bot.guilds.get("559491866207453184").channels.get("559491866207453186").send(eMessage);
 		message.delete();
