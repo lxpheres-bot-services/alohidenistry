7ef423c4de964aaf448fc7b3d58916625a8e8b3c
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-05-28 15:43:29 -0400

    Update generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
 const Discord = require("discord.js");
 	const allowedid = ["293060399106883584"];
-	if (allowedid.includes(message.author.id) return message.reply("No.");
+	if (allowedid.includes(message.author.id);
  function makeid(length) {
    var result           = 'PREMIUM-';
    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
