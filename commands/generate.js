b90b5eadd6ba39256d8fbaed4374bd939b185f1e
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-05-27 21:31:58 -0400

    Update generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const allowedid = ["293060399106883584"];
-	if (allowedid.includes(message.author.id)) {
 function makeid(length) {
    var result           = 'PREMIUM-';
    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
