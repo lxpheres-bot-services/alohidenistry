7cd7ee0f11cca6581c0dd3486ac9e82315208785
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-08-23 20:37:44 -0400

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
+	if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You cannot run this command. You must be a SHR+!");
 	let embed = new Discord.RichEmbed()
 		.setTitle("Server Announcement")
 		.setColor("#ffeb5c")
 		.setDescription(args.join(" "))
 		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL);
-	bot.channels.get("535492440874811392").send({embed: embed}).then(() => {
+	bot.channels.get("613900389137514515").send({embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
 		message.reply("Something went wrong when announcing, please check my permissions and try again.");
@@ -18,6 +18,6 @@ module.exports.help = {
 	usage: "announce <txt>",
 	description: "Announcement command, no ping.",
 	longdes: "An announcement command, with no ping.",
-	mentionedperm: "MANAGE_ROLES",
+	mentionedperm: "ADMINISTRATOR",
 	category: "Utility"
 };
