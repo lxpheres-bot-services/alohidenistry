15e7a4305dec0ca5db9497ea06bc433a79c67f5f
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-08-24 18:49:12 -0400

    Update new.js

--- commands/new.js
+++ commands/new.js
@@ -31,13 +31,5 @@ fs.writeFile('./tickets.json', '{\n"id":' + data.id + "\n}", (err) => {
     if (!err) return;
     console.error(err)
   });
- module.exports.help = {
-	name: "new",
-	usage: "new (reason)",
-	description: "nil",
-	longdes: "Make a ticket.",
-	mentionedperm: "N/A",
-	category: "Utility"
-};
  	}}
   
