fc3b35704d06af7b9e5f5220c39c4e71933184a3
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-09-08 18:03:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ module.exports.run = async () => {
 	});
 
 	client.on("ready", async () => {
-		client.user.setPresence({ game: { type: 3, name: "Trivago Hotels! | Run ;help!" }, status: "online" });
+		client.user.setPresence({ game: { type: 3, name: "Alohi Dentistry | Run ;help!" }, status: "online" });
 		console.log(`${client.user.tag} has started!`);
 		// await noblox.cookieLogin(process.env.roblox);
 		//rbx.login({ username: "CreamyzRankingBot", password: process.env.rblx});
