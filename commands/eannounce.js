2f607f9b54755ce89d60856250aed06a9b6781fc
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-09-08 17:54:25 -0400

    Update eannounce.js

--- commands/eannounce.js
+++ commands/eannounce.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You cannot run this command. You must be a HR+!");
+	if (!message.member.hasPermission("MANAGE_SERVER")) return message.reply("You cannot run this command. You must be a HR+!");
 	let embed = new Discord.RichEmbed()
-		.setTitle("Server Announcement")
+		.setTitle("Alohi Dentistry Server Announcement")
 		.setColor("#ffeb5c")
 		.setDescription(args.join(" "))
 		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL);
-	bot.channels.get("535492440874811392").send("@everyone", {embed: embed}).then(() => {
+	bot.channels.get("579722437437030422").send("@everyone", {embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
 		message.reply("Something went wrong when announcing, please check my permissions and try again.");
