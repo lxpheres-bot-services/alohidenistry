280d343ce89bfc2ecdbf52e3a5c9205b0d785f7f
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-09-08 17:53:58 -0400

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You cannot run this command. You must be a SHR+!");
+	if (!message.member.hasPermission("MANAGE_SERVER")) return message.reply("You cannot run this command. You must be a SR+!");
 	let embed = new Discord.RichEmbed()
-		.setTitle("TH | Server Announcement")
+		.setTitle("Alohi Dentistry | Server Announcement")
 		.setColor("#ffeb5c")
 		.setDescription(args.join(" "))
 		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL)
 		.setThumbnail("https://cdn.discordapp.com/attachments/613900389137514515/614440651882364979/image0.png");
-	bot.channels.get("613900389137514515").send({embed: embed}).then(() => {
+	bot.channels.get("579722437437030422").send({embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
 		message.reply("Something went wrong when announcing, please check my permissions and try again.");
@@ -19,6 +19,6 @@ module.exports.help = {
 	usage: "announce <txt>",
 	description: "Announcement command, no ping.",
 	longdes: "An announcement command, with no ping.",
-	mentionedperm: "ADMINISTRATOR",
+	mentionedperm: "Bot Perms Role",
 	category: "Utility"
 };
