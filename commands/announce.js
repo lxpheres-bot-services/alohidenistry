21c15747f589865fd5fcb2cd810f9a1a2f5a3cc6
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-09-08 17:55:30 -0400

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 		.setColor("#ffeb5c")
 		.setDescription(args.join(" "))
 		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL)
-		.setThumbnail("https://cdn.discordapp.com/attachments/613900389137514515/614440651882364979/image0.png");
+		.setThumbnail("https://cdn.discordapp.com/attachments/620275379520667648/620373417631154221/b159c0540d79a99026f2e154ff447e58.png");
 	bot.channels.get("579722437437030422").send({embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
