9530a8c895a0f1fc03c6f01c6089ab8c4815a4a9
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-09-10 15:39:04 -0400

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,7 +1,6 @@
-const discord = require("discord.js");
 var rbx = require("noblox.js");
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.roles.get("620375972818059273")) return message.reply("Invalid permissons! You must have the `Bot Perms` role.").catch(() => bot.safeSend(message, module.exports.help.name));
+	if (!message.member.roles.get("620375972818059273")) return message.reply("Invalid permissons! You must have the `Bot Admin` role.").catch(() => bot.safeSend(message, module.exports.help.name));
 	if (!args[1]) return message.reply("Usage: `;rank (username) (rank)`").catch(() => bot.safeSend(message, module.exports.help.name));
 	rbx.getRoles(4931059).then((roles) => {
 		rbx.getIdFromUsername(args[0]).then((result) => {
@@ -9,10 +8,10 @@ module.exports.run = async (bot, message, args) => {
 				if (currentRank === 0) return message.reply("This person isn't in the group!").catch(() => bot.safeSend(message, module.exports.help.name));
 				var rank = args.splice(1, args.length).join(" ");
 				if (!roles.find((role) => role.Name === rank)) return message.reply("Not a valid rank name!").catch(() => bot.safeSend(message, module.exports.help.name));
-				if (!roles.find((role) => role.Name === rank).Rank > roles.find((role) => role.Name === "Head of Directors").Rank || currentRank > roles.find((role) => role.Name === "Head of Directors").Rank) return message.reply("You cannot change the rank of a HoD, or anyone above the rank of one!.").catch(() => bot.safeSend(message, module.exports.help.name));
+				if (roles.find((role) => role.Name === rank).Rank > roles.find((role) => role.Name === "Head of Directors").Rank || currentRank > roles.find((role) => role.Name === "Head of Directors").Rank) return message.reply("You cannot change the rank of a Human Resources Officer+, or rank anyone above Human Resources Officer").catch(() => bot.safeSend(message, module.exports.help.name));
 				rbx.setRank(4931059, result, roles.find((role) => role.Name === rank).Rank).then(async () => {
 					message.reply(`Ranked \`${args[0]}\` to \`${rank}\``).catch(() => bot.safeSend(message, module.exports.help.name));
-					//await bot.channels.get("539898170705051651").send(`Alert! Since ${message.author.tag} requested, I have ranked \`${args[0]}\` to \`${rank}\`!`);
+					// await bot.channels.get("506303108998234133").send(`Alert! Since ${message.author.tag} requested, I have ranked \`${args[0]}\` to \`${rank}\`!`);
 				}).catch((e) => {
 					console.warn(e);
 					message.reply("Couldn't rank this user in the group.").catch(() => bot.safeSend(message, module.exports.help.name));
@@ -26,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
 			message.reply("Couldn't find this user.").catch(() => bot.safeSend(message, module.exports.help.name));
 		});
 	}).catch((e) => {
-console.warn(e);
+		console.warn(e);
 		message.reply("Couldn't fetch group ranks.").catch(() => bot.safeSend(message, module.exports.help.name));
 	});
 },
